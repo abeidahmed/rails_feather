55cdecd5929ceb9f27e132c5473d903fb16913f6
abeidahmed <abeidahmed92@gmail.com>
2021-03-15 14:10:27 +0530

    Initialize rails-feather

---
+++ lib/rails/feather.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "feather/version"
+
+module Rails
+  module Feather
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
