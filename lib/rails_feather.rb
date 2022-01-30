a0df36e5dd9206392504bbd79759540618803bf1
Emir da Ponte <emir.daponte@gmail.com>
2022-01-29 22:04:03 -0300

    Avoid reading from disk for rendering each icon

--- lib/rails_feather.rb
+++ lib/rails_feather.rb
@@ -1,7 +1,10 @@
+require "json"
+
 require_relative "rails_feather/version"
 require_relative "rails_feather/rails_feather"
 require_relative "rails_feather/railtie" if defined?(Rails)
 
 module RailsFeather
-  ICON_PATH = File.join(File.dirname(__FILE__), "..", "compressed")
+  ICONS_PATH = File.join(File.dirname(__FILE__), "..", "compressed", "icons.json")
+  ICONS_HASH = JSON.load_file(ICONS_PATH)
 end
